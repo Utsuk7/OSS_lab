3814aa4f3b70829bcb6cf4b354e151601edc9eb8
Chirag Tyagi <95204380+chiragtyagi2003@users.noreply.github.com>
2023-07-31 15:38:20 +0530

    lab1

---
+++ program.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+
+     for(int i = 0;i<35;i++)
+    {
+
+        cout<<"*";
+    }
+
+    cout<<endl<<endl;
+
+    cout <<" Hello Github, this is Chirag Tyagi"<<endl;
+
+    cout<<endl;
+
+    for(int i = 0;i<35;i++)
+    {
+
+        cout<<"*";
+    }
+
+    return 0;
+}
